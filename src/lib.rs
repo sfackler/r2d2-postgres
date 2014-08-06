69097031faedf987feab2f1aa3ca9dec7374d152
Steven Fackler <sfackler@gmail.com>
2014-08-05 23:02:36 -0700

    Add PostgresPoolManager

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+extern crate r2d2;
+extern crate postgres;
+
+use postgres::{PostgresConnection, PostgresConnectParams, IntoConnectParams, SslMode};
+use postgres::error::PostgresConnectError;
+
+pub struct PostgresPoolManager {
+    params: PostgresConnectParams,
+    ssl_mode: SslMode,
+}
+
+impl PostgresPoolManager {
+    pub fn new<T: IntoConnectParams>(params: T, ssl_mode: SslMode)
+                                     -> Result<PostgresPoolManager, PostgresConnectError> {
+        Ok(PostgresPoolManager {
+            params: try!(params.into_connect_params()),
+            ssl_mode: ssl_mode,
+        })
+    }
+}
+
+impl r2d2::PoolManager<PostgresConnection, PostgresConnectError> for PostgresPoolManager {
+    fn connect(&self) -> Result<PostgresConnection, PostgresConnectError> {
+        PostgresConnection::connect(self.params.clone(), &self.ssl_mode)
+    }
+}
---
+++ tests/test.rs
@@ -0,0 +1,56 @@
+extern crate postgres;
+extern crate r2d2;
+extern crate r2d2_postgres;
+
+use std::default::Default;
+use std::sync::{Arc, Future};
+use std::comm;
+
+use postgres::NoSsl;
+use postgres::error::SocketError;
+use r2d2_postgres::PostgresPoolManager;
+
+#[test]
+fn test_bad_url_error() {
+    let manager = PostgresPoolManager::new("postgres://bogushost", NoSsl).unwrap();
+    let config = Default::default();
+    match r2d2::Pool::new(config, manager) {
+        Err(r2d2::ConnectionError(SocketError(_))) => {}
+        Err(err) => fail!("Unexpected error {}", err),
+        _ => fail!("Unexpected success")
+    }
+}
+
+#[test]
+fn test_basic() {
+    let manager = PostgresPoolManager::new("postgres://postgres@localhost", NoSsl).unwrap();
+    let config = r2d2::Config {
+        initial_size: 2,
+        ..Default::default()
+    };
+    let pool = Arc::new(r2d2::Pool::new(config, manager).unwrap());
+
+    let (s1, r1) = comm::channel();
+    let (s2, r2) = comm::channel();
+
+    let pool1 = pool.clone();
+    let mut fut1 = Future::spawn(proc() {
+        let conn = pool1.get().unwrap();
+        s1.send(());
+        r2.recv();
+        conn.replace();
+    });
+
+    let pool2 = pool.clone();
+    let mut fut2 = Future::spawn(proc() {
+        let conn = pool2.get().unwrap();
+        s2.send(());
+        r1.recv();
+        conn.replace();
+    });
+
+    fut1.get();
+    fut2.get();
+
+    pool.get().unwrap().replace();
+}
