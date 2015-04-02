b005922c98012f00b4752bae1b14385b11a0c4cb
Steven Fackler <sfackler@gmail.com>
2015-04-02 18:58:03 +0000

    rebuild pages at b9ebb2f13f8d03ffe3033306db50ecef001a618f

--- doc/r2d2_postgres/sidebar-items.js
+++ doc/r2d2_postgres/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","A unified enum of errors returned by postgres::Connection"]],"struct":[["PostgresConnectionManager","An `r2d2::ConnectionManager` for `postgres::Connection`s."]]});
+initSidebarItems({"struct":[["PostgresConnectionManager","An `r2d2::ConnectionManager` for `postgres::Connection`s."]],"enum":[["Error","A unified enum of errors returned by postgres::Connection"]]});
