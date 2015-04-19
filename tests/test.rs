1dc187a5e486902aa7d5b75abe7fc59699ce01b5
Steven Fackler <sfackler@gmail.com>
2015-04-18 17:31:58 -0700

    Fix tests to avoid scoped

--- tests/test.rs
+++ tests/test.rs
@@ -20,7 +20,7 @@ fn test_basic() {
     let (s2, r2) = mpsc::channel();
 
     let pool1 = pool.clone();
-    let t1 = thread::scoped(move || {
+    let t1 = thread::spawn(move || {
         let conn = pool1.get().unwrap();
         s1.send(()).unwrap();
         r2.recv().unwrap();
@@ -28,15 +28,15 @@ fn test_basic() {
     });
 
     let pool2 = pool.clone();
-    let t2 = thread::scoped(move || {
+    let t2 = thread::spawn(move || {
         let conn = pool2.get().unwrap();
         s2.send(()).unwrap();
         r1.recv().unwrap();
         drop(conn);
     });
 
-    t1.join();
-    t2.join();
+    t1.join().unwrap();
+    t2.join().unwrap();
 
     pool.get().unwrap();
 }
