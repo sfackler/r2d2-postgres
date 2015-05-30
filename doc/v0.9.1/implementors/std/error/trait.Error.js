26b926223a792c8f217f0e963909ea0f791bf065
Steven Fackler <sfackler@gmail.com>
2015-05-30 12:52:02 -0700

    Release v0.9.1

---
+++ doc/v0.9.1/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['r2d2_postgres'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='r2d2_postgres/enum.Error.html' title='r2d2_postgres::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
