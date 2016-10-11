da6b91c1664bef7ecb6490edb12b4f539f610433
Steven Fackler <sfackler@gmail.com>
2016-10-11 13:24:59 -0700

    Release v0.11.0

---
+++ doc/v0.11.0/implementors/r2d2/trait.ManageConnection.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["r2d2_postgres"] = ["impl <a class='trait' href='https://sfackler.github.io/r2d2/doc/v0.7.0/r2d2/trait.ManageConnection.html' title='r2d2::ManageConnection'>ManageConnection</a> for <a class='struct' href='r2d2_postgres/struct.PostgresConnectionManager.html' title='r2d2_postgres::PostgresConnectionManager'>PostgresConnectionManager</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
