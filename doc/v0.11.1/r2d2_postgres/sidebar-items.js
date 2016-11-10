dded0b7fe47ed52b8b19db5ecc205d2356d05b6d
Steven Fackler <sfackler@palantir.com>
2016-11-10 16:57:06 +0000

    Release v0.11.1

---
+++ doc/v0.11.1/r2d2_postgres/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Error","A unified enum of errors returned by postgres::Connection"],["TlsMode","Like `postgres::TlsMode` except that it owns its `TlsHandshake` instance."]],"struct":[["PostgresConnectionManager","An `r2d2::ManageConnection` for `postgres::Connection`s."]]});
